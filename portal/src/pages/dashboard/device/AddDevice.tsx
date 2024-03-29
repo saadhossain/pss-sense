a2090bef81a76cccc8663c835e1cab1376015b24
saadhossain <mshsarkar@gmail.com>
2024-03-30 01:40:32 +0600

    Add Device form modified

--- portal/src/pages/dashboard/device/AddDevice.tsx
+++ portal/src/pages/dashboard/device/AddDevice.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import toast from 'react-hot-toast';
 import ButtonLoader from '../../../components/Loader/ButtonLoader';
 import { DataContext, DataContextType } from '../../../context/AuthProvider';
@@ -9,30 +9,41 @@ const AddDevice = () => {
     const { processing, setProcessing } = useContext(
         DataContext
     ) as DataContextType;
+
+    //Set email
+    const [serial, setSerial] = useState();
+    //Get the email from the input
+    const handleSerial = (e: any) => {
+        setSerial(e.target.value)
+    }
+    console.log(serial);
     //Register a new user
     const handleRegistration = async (e: any) => {
         e.preventDefault();
         setProcessing(true);
+        if (!serial) {
+            toast.error('Please fill the the form');
+            setProcessing(false);
+            return;
+        }
         //Get the form values
         const form = e.target;
-        const fullName = form.name.value;
+        const deviceName = form.name.value;
         const password = form.password.value;
         const phone = form.phone.value;
         //Handle Profile Picture upload
         const image = form.profile.files[0];
         const profileImg = await uploadFiletoFirebase('profileImage', image);
-        if (profileImg) {
-            const userInfo = {
-                fullName,
-                phone,
-                password,
-                profileImg
-            };
-            // Save new user to Database
-            form.reset();
-            setProcessing(false);
-            toast.success('New User added...');
-        }
+        const userInfo = {
+            deviceName,
+            phone,
+            password,
+            profileImg
+        };
+        // Save new user to Database
+        form.reset();
+        setProcessing(false);
+        toast.success('New User added...');
     }
     //Save New user to the database
     const saveUserToDB = (userInfo: UserInfoType) => {
@@ -52,39 +63,38 @@ const AddDevice = () => {
                     <form onSubmit={handleRegistration} className="space-y-12 ng-untouched ng-pristine ng-valid">
                         <div className="space-y-4">
                             <div>
-                                <label htmlFor="name" className="block mb-2 text-lg">PSS-QR Code/Serial Number</label>
-                                <input type="text" name="name" id="name" placeholder="Full Name" className="w-full px-3 py-2 border rounded-md border-gray-800 text-gray-800" />
+                                <label htmlFor="serial" className="block mb-2 text-lg">PSS-QR Code/Serial Number</label>
+                                <input onChange={handleSerial} type="text" name="serial" id="serial" placeholder="Enter Serial/QR Code" className="w-full px-3 py-2 border rounded-md border-gray-800 text-gray-800" />
                             </div>
                             <div>
-                                <label htmlFor="devictype" className="block mb-2 text-lg">Device type</label>
-                                <select id="devictype" className="w-full px-3 py-2 border rounded-md border-gray-800 text-gray-800">
-                                    <option value="Select Type">Select Type</option>
-                                    <option value="Select Type">Select Type</option>
-                                    <option value="Select Type">Select Type</option>
-                                    <option value="Select Type">Select Type</option>
-                                    <option value="Select Type">Select Type</option>
-                                </select>
+                                <label htmlFor="name" className="block mb-2 text-lg">Device ID</label>
+                                <input type="text" name="name" id="name" placeholder="Enter ID" className="w-full px-3 py-2 border rounded-md border-gray-800 text-gray-800" />
                             </div>
                             <div>
-                                <label htmlFor="name" className="block mb-2 text-lg">PSS-QR Code</label>
-                                <input type="text" name="name" id="name" placeholder="Full Name" className="w-full px-3 py-2 border rounded-md border-gray-800 text-gray-800" />
+                                <label htmlFor="name" className="block mb-2 text-lg">Device Name</label>
+                                <input type="text" name="name" id="name" placeholder="Enter Device Name" className="w-full px-3 py-2 border rounded-md border-gray-800 text-gray-800" />
                             </div>
                             <div>
-                                <label htmlFor="name" className="block mb-2 text-lg">Devic ID</label>
-                                <input type="text" name="name" id="name" placeholder="Full Name" className="w-full px-3 py-2 border rounded-md border-gray-800 text-gray-800" />
+                                <label htmlFor="name" className="block mb-2 text-lg">Customer Device Name</label>
+                                <input type="text" name="name" id="name" placeholder="Customer Device Name" className="w-full px-3 py-2 border rounded-md border-gray-800 text-gray-800" />
                             </div>
                             <div>
-                                <label htmlFor="name" className="block mb-2 text-lg">Devic Name</label>
-                                <input type="text" name="name" id="name" placeholder="Full Name" className="w-full px-3 py-2 border rounded-md border-gray-800 text-gray-800" />
+                                <label htmlFor="name" className="block mb-2 text-lg">Customer ID</label>
+                                <input type="text" name="name" id="name" placeholder="Customer ID" className="w-full px-3 py-2 border rounded-md border-gray-800 text-gray-800" />
                             </div>
-
-
-
+                            <div>
+                                <label htmlFor="name" className="block mb-2 text-lg">Device Network Password </label>
+                                <input type="text" name="name" id="name" placeholder="Device Network Password " className="w-full px-3 py-2 border rounded-md border-gray-800 text-gray-800" />
+                            </div>
+                            <div>
+                                <label htmlFor="name" className="block mb-2 text-lg">External IP address  </label>
+                                <input type="text" name="name" id="name" placeholder="External IP address  " className="w-full px-3 py-2 border rounded-md border-gray-800 text-gray-800" />
+                            </div>
                         </div>
                         <div className="space-y-2">
                             <div>
                                 {/* Adding User Button */}
-                                <button type="submit" className={`w-full px-8 py-3 font-semibold rounded-md bg-gray-800 text-white`}>{processing ? <ButtonLoader title='Adding User' /> : 'Add User'}</button>
+                                <button type="submit" className={`w-full px-8 py-3 font-semibold rounded-md bg-gray-800 text-white`}>{processing ? <ButtonLoader title='Adding Device' /> : 'Add Device'}</button>
                             </div>
                         </div>
                     </form>
